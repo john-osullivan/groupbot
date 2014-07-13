46595626eaae37597d0bfc48a0ed66943867d187
John O'Sullivan <johno@mit.edu>
2014-07-12 21:35:14 -0400

    Updated engine URL.

--- models.py
+++ models.py
@@ -5,7 +5,7 @@ from sqlalchemy import Column, Integer, String, ForeignKey, Boolean, DateTime
 from sqlalchemy.engine import reflection
 from app import db
 
-engine = create_engine('postgresql://john')
+engine = create_engine(os.environ['DATABASE_URL'])
 inspector = reflection.Inspector.from_engine(engine)
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
